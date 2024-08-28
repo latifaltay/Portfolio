6dc43eb16e1a8602ae0a449e17a2bc054131e90b
Latif Altay <latifaltay00@gmail.com>
2024-08-28 03:10:39 +0300

    log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -37,7 +37,7 @@ function Home() {
   
         // Bilgileri .NET API'ye gönder
         try {
-          const response = await fetch('http://localhost/api/LogUserInfo', { // Burada localhost kullanıyoruz
+          const response = await fetch('https://localhost:443/api/UserInfo/LogUserInfo', { // Burada localhost kullanıyoruz
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
